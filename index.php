5cefcce13b0ae3fae8fd0829c5e87f82f785c3f5
Pieter <pieterpretorius223@gmail.com>
2020-09-11 23:00:26 +0200

    break & continue

--- index.php
+++ index.php
@@ -1,33 +1,27 @@
 <?php 
 
-  // Conditional Statements
+  // Continue & break
   // =====================================
-  // $price = 20;
-  // if ($price < 10) {
-  //   echo 'the condition is met';
-  // } elseif ($price < 30) {
-  //   echo 'elseif condition met';
-  // } else {
-  //   echo 'condition not met';
-  // }
 
   $products = [
     ['name' => 'shiny star', 'price' => 20],
     ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red  shell', 'price' => 15],
+    ['name' => 'red shell', 'price' => 15],
     ['name' => 'gold coin', 'price' => 5],
     ['name' => 'lightning bolt', 'price' => 40],
     ['name' => 'banana skin', 'price' => 2]
   ];
 
-  // foreach ($products as $product) {
-    // if ($product['price'] < 15 && $product['price'] > 2) {
-    //   echo $product['name'] . '<br />';
-    // }
-  //   if ($product['price'] < 20 || $product['price'] < 10) {
-  //     echo $product['name'] . '<br />';
-  //   }
-  // }
+  foreach ($products as $product) {
+    if($product['name'] === 'lightning bolt') {
+    break; // break out of loop - stop at this statement
+    }
+
+    if($product['price'] > 15) {
+      continue; // skip the rest of the code from this point and conitnue with the next value in the loop
+    }
+    echo $product['name'] . '<br />';
+  }
 
 ?>
 
@@ -39,16 +33,6 @@
   <title>My first PHP file</title>
 </head>
 <body>
-
-  <div>
-    <ul>
-      <?php foreach ($products as $product) { ?>
-        <?php if($product['price'] > 15) { ?>
-          <li><?php echo $product['name']; ?></li>
-        <?php } ?>
-      <?php } ?>
-    </ul>
-  </div>
 
 </body>
 </html>
