2f4fb8906912936ffbd0e1e069a064056c2554a2
Pieter <pieterpretorius223@gmail.com>
2020-09-12 13:53:11 +0200

    project header and footer

--- index.php
+++ index.php
@@ -1,33 +1,14 @@
 <?php 
 
-  // include - carry on with rest of php after errer
-  // ===================================================================
 
-  include('ninjas.php');
-  include 'ninjas.php';
-
-  echo 'end of php';
-
-  // require - fatal error stops the code
-  // ===================================================================
-
-  require('ninjas.php');
-  require 'ninjas.php';
-
-  echo 'end of php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My first PHP file</title>
-</head>
-<body>
 
-<?php include('content.php') ?>
+  <?php include('templates/header.php') ?>
 
-</body>
+  <?php include('templates/footer.php') ?>
+
 </html>
