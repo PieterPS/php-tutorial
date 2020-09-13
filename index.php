6277c9d59ff3d70fa8f6e13505134b8fe4ef5a09
Pieter <pieterpretorius223@gmail.com>
2020-09-13 17:10:50 +0200

    connecting to a database

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
 
+  //connect to database
+  $conn = mysqli_connect('localhost', 'pieter', 'test1234', 'ninja_pizza');
 
+  //check connection
+  if(!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+  }
 
 ?>;
 
