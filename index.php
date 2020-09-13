ab0e9e09d49b496c5ee4ed1d98822fef368e2bc6
Pieter <pieterpretorius223@gmail.com>
2020-09-13 18:17:31 +0200

    rendering data to the browser

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     echo 'Connection error: ' . mysqli_connect_error();
   }
 
-  // write query for all pizzas
-  $sql = 'SELECT title, ingredients, id FROM pizzas';
+  // write query for all pizzas - ordered by created_at date
+  $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
   // make query & get result
   $result = mysqli_query($conn, $sql);
@@ -23,14 +23,36 @@
   // close connection
   mysqli_close($conn);
 
-  print_r($pizzas);
-
 ?>;
 
 <!DOCTYPE html>
 <html lang="en">
 
   <?php include('templates/header.php') ?>
+
+  <h4 class="center grey-text">Pizzas!</h4>
+
+  <div class="container">
+    <div class="row">
+
+      <?php foreach($pizzas as $pizza) { ?>
+
+        <div class="col s6 m4">
+          <div class="card z-depth-0">
+            <div class="card-content center">
+              <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+              <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+            </div>
+            <div class="card-action right-align">
+              <a href="#" class="brand-text">more info</a>
+            </div>
+          </div>
+        </div>
+
+      <?php } ?>
+
+    </div>
+  </div>
 
   <?php include('templates/footer.php') ?>
 
